c24c80f84165c595059b64b05585d92f32c61acd
Nathan Langridge <nathanlangridge@hotmail.com>
2019-10-11 11:34:21 +0100

    films list & film detail added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,40 @@
 <template lang="html">
+  <div>
+    <h1>Studio Ghibli Films</h1>
 
+    <films-list :films='films'></films-list>
+    <film-detail :film='selectedFilm'></film-detail>
+
+  </div>
 </template>
 
 <script>
+import {eventBus} from "./main.js";
+import FilmsList from "./components/FilmsList.vue";
+import FilmDetail from "./components/FilmDetail.vue";
+// import ListItem from "./components/ListItem.vue";
+
 export default {
+  name: 'app',
+  data(){
+    return {
+      films: [],
+      selectedFilm: null
+    };
+  },
+  mounted(){
+    fetch('https://ghibliapi.herokuapp.com/films/')
+    .then(result => result.json())
+    .then(films => this.films = films)
+
+    eventBus.$on('film-selected', (film) => {
+      this.selectedFilm = film;
+    })
+  },
+  components: {
+    "films-list": FilmsList,
+    "film-detail": FilmDetail
+  }
 }
 </script>
 
---
+++ src/components/FilmDetail.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <div v-if='film'>
+    <h3>{{film.title}}</h3>
+
+    <p>Director: {{film.director}}</p>
+    <p>Year: {{film.release_date}}</p>
+    <p>Critical Reception: {{film.rt_score}}%<p>
+    <p>Description: {{film.description}}</p>
+
+  </div>
+</template>
+
+<script>
+import {eventBus} from "../main.js"
+
+export default {
+  name: 'film-detail',
+  props: ['film']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
